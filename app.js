df942a28a589a4df08d69eef1e2b5ad754b0d9fe
Chris Matthieu <chris@matthieu.us>
2014-01-12 19:37:39 -0700

    fixed nodejitsu port

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get('/available', function(req, res){
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
